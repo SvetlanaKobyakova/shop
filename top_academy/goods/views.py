720e8bfa4cd7dfcc61c7d4a28641ffbf48d71ec1
SvetlanaKobyakova <kobyakasv@mail.ru>
2025-07-08 14:30:53 +0300

    0807

--- top_academy/goods/views.py
+++ top_academy/goods/views.py
@@ -3,11 +3,18 @@ from .models import Products
 
 def catalog(request):
     goods = Products.objects.all()
+
+
+
+
     context = {
         'goods': goods
     }
     return render(request, "goods/catalog.html", context)
 
 def product(request):
-
-    return render(request, )
+#     product = Products.objects.get(slug=slug)
+#     context = {
+#         'product': product
+#     }
+    return render(request, 'goods/product.html')
